3a6827af8c11413278f7dca411ba724c14861f5b
unknown <lethuzulu345@gmail.com>
2024-08-23 21:00:56 +0200

    refactor lib functions. add compare_size function.

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,18 @@ use serde::{Deserialize, Serialize};
 use std::cmp::Eq;
 use std::cmp::Ord;
 use std::cmp::Ordering;
-use std::cmp::Reverse;
-use std::collections::binary_heap;
 use std::collections::{BTreeMap, BinaryHeap, HashMap};
-use std::env::args;
 use std::fmt::Debug;
+use std::fs;
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::Error;
 use std::io::Read;
 use std::io::Write;
+use std::os::windows::fs::MetadataExt;
 use std::path::Path;
-use std::process::Output;
+
+
 
 const DELIMITER: u8 = 0xFF; // Delimiter to separate tree from compressed data
 
@@ -49,13 +49,6 @@ impl HuffmanTree {
             HuffmanTree::LeafNode { weight, .. } => *weight,
         }
     }
-
-    fn value(&self) -> Option<char> {
-        match self {
-            HuffmanTree::LeafNode { element, .. } => Some(*element),
-            HuffmanTree::InternalNode { .. } => None,
-        }
-    }
 }
 
 impl Ord for HuffmanTree {
@@ -87,21 +80,22 @@ impl Eq for HuffmanTree {}
 pub fn read_file(input: &String) -> Result<String, Error> {
     let input_path = Path::new(input);
     let mut file = OpenOptions::new().read(true).open(input_path)?;
-
     let mut contents = String::new();
-
-    let contents_length = file.read_to_string(&mut contents)?;
+    let _ = file.read_to_string(&mut contents)?;
     Ok(contents)
 }
 
-pub fn generate_frequency_table(string: &String) -> HashMap<char, i32> {
+pub fn generate_frequency_table(string: &String) -> Result<HashMap<char, i32>, &'static str> {
+    if string.is_empty() {
+        return Err("Input String is Empty.");
+    }
     let mut frequency_table: HashMap<char, i32> = HashMap::new();
     for char in string.chars() {
         let entry = frequency_table.entry(char);
         let value = entry.or_insert(0);
         *value += 1;
     }
-    frequency_table
+    Ok(frequency_table)
 }
 
 fn build_tree(binary_heap: &mut BinaryHeap<HuffmanTree>) {
@@ -117,7 +111,12 @@ fn build_tree(binary_heap: &mut BinaryHeap<HuffmanTree>) {
     }
 }
 
-pub fn generate_huffman_tree(frequency_table: &HashMap<char, i32>) -> Option<HuffmanTree> {
+pub fn generate_huffman_tree(
+    frequency_table: &HashMap<char, i32>,
+) -> Result<HuffmanTree, &'static str> {
+    if frequency_table.is_empty() {
+        return Err("Frequency table is empty.");
+    }
     let mut binary_heap: BinaryHeap<HuffmanTree> = BinaryHeap::new(); //priority queue
 
     for (char, count) in frequency_table.iter() {
@@ -128,7 +127,7 @@ pub fn generate_huffman_tree(frequency_table: &HashMap<char, i32>) -> Option<Huf
     build_tree(&mut binary_heap);
 
     let huffman_tree = binary_heap.pop();
-    huffman_tree
+    huffman_tree.ok_or("Failed to build Huffma Tree")
 }
 
 pub fn generate_huffman_code(huffman_tree: &HuffmanTree) -> BTreeMap<char, String> {
@@ -138,14 +137,10 @@ pub fn generate_huffman_code(huffman_tree: &HuffmanTree) -> BTreeMap<char, Strin
         prefix: String,
     ) {
         match huffman_tree {
-            HuffmanTree::LeafNode { weight, element } => {
+            HuffmanTree::LeafNode { element, .. } => {
                 encoding_table.insert(*element, prefix);
             }
-            HuffmanTree::InternalNode {
-                weight,
-                right,
-                left,
-            } => {
+            HuffmanTree::InternalNode { right, left, .. } => {
                 recurse(&left, encoding_table, prefix.clone() + "0");
                 recurse(&right, encoding_table, prefix + "1");
             }
@@ -217,28 +212,46 @@ pub fn serialize_tree(huffman_tree: &HuffmanTree) -> Vec<u8> {
     serialized_tree
 }
 
-pub fn deserialize_tree(serialized_tree: &Vec<u8>) -> HuffmanTree {
-    let deserialized_tree: HuffmanTree = bincode::deserialize(serialized_tree).unwrap();
+pub fn serialize_prefix_table(encoding_table: &BTreeMap<char, String>) -> Vec<u8> {
+    let serialized_table = bincode::serialize(encoding_table).unwrap();
+    serialized_table
+}
 
+pub fn deserialize_tree(serialized_tree: &[u8]) -> HuffmanTree {
+    let deserialized_tree: HuffmanTree = bincode::deserialize(serialized_tree).unwrap();
+    deserialized_tree
+}
+
+pub fn deserialize_prefix_table(serialized_prefix_table: &[u8]) -> HashMap<char, String> {
+    let deserialized_tree: HashMap<char, String> =
+        bincode::deserialize(serialized_prefix_table).unwrap();
     deserialized_tree
 }
 
 pub fn write_output(
     output: &String,
     huffman_tree: &HuffmanTree,
-    encoded_data: &str,
-    length: usize,
-) {
+    input_string: &String,
+    encoding_table: &BTreeMap<char, String>,
+) -> Result<(), std::io::Error> {
+
+    //TODO: Consider separtaing encoding part and writing part into two different fns.
     let output_path = Path::new(output);
-    let mut output_file = File::create(output_path).unwrap();
+    let mut output_file = File::create(output_path)?;
 
     let serialized_tree = serialize_tree(huffman_tree);
 
-    output_file.write_all(&serialized_tree).unwrap();
-    output_file.write_all(&[DELIMITER]).unwrap();
+    // let serialized_prefix_table = serialize_prefix_table(encoding_table);
 
-    let packed_bytes = pack_bits(encoded_data, length);
-    output_file.write_all(&packed_bytes).unwrap();
+    let (encoded_string, length) = encode_text(&input_string, &encoding_table);
+    let encoded_string_bytes = pack_bits(&encoded_string, length);
+
+    output_file.write_all(&serialized_tree)?;
+    // output_file.write_all(&serialized_prefix_table)?;
+    output_file.write_all(&[DELIMITER])?;
+
+    output_file.write_all(&encoded_string_bytes)?;
+    Ok(())
 }
 
 pub fn read_output(output: &String) -> Vec<u8> {
@@ -251,21 +264,23 @@ pub fn read_output(output: &String) -> Vec<u8> {
     let delimiter_pos = contents.iter().position(|&x| x == DELIMITER).unwrap();
 
     let tree_bytes = &contents[..delimiter_pos];
-    let huffman_tree: HuffmanTree = bincode::deserialize(tree_bytes).unwrap();
+    let encoded_bytes = &contents[delimiter_pos + 1..];
 
-    let (bit_string, length) = unpack_bits(&contents[delimiter_pos + 1..]);
 
-    let s = decode_data(&bit_string, &huffman_tree);
-    println!(" s {}", s);
-
+    decode_data(&tree_bytes, &encoded_bytes);
     contents
 }
 
-fn decode_data(bit_string: &str, huffman_tree: &HuffmanTree) -> String {
+fn decode_data(tree_bytes: &[u8], encoded_bytes: &[u8]) -> String {
+
+    let huffman_tree: HuffmanTree = deserialize_tree(tree_bytes);
+
+    let (bit_string, _) = unpack_bits(encoded_bytes);
+
     let mut decoded_string = String::new();
     let mut current_code = String::new();
 
-    let encoding_table = generate_huffman_code(huffman_tree);
+    let encoding_table = generate_huffman_code(&huffman_tree);
 
     let reverse_table: HashMap<String, char> =
         encoding_table.into_iter().map(|(k, v)| (v, k)).collect();
@@ -278,4 +293,22 @@ fn decode_data(bit_string: &str, huffman_tree: &HuffmanTree) -> String {
         }
     }
     decoded_string
+}
+
+pub fn compare_sizes(input: &str, output: &str){
+    let input_metadata = fs::metadata(input).unwrap();
+    let input_size = input_metadata.file_size();
+    println!("Input File Size {:.2} KB", input_size as f64 / 1024.0);
+
+
+    let output_metadata = fs::metadata(output).unwrap();
+    let output_size = output_metadata.file_size();
+    println!("Compressed File Size {:.2} KB", output_size as f64 / 1024.0);
+
+    let size_saved = input_size as f64 - output_size as f64;
+
+    let percentage_saved = (size_saved / input_size as f64) * 100.0;
+
+    println!("Size saved: {:.2} KB", size_saved / 1024.0);
+    println!("Percentage saved: {:.2}%", percentage_saved);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,48 @@
-use std::collections::{BTreeMap, BinaryHeap, HashMap};
 use std::env::args;
 mod config;
 use config::Config;
-use lib::{encode_text, pack_bits, unpack_bits};
 
 mod lib;
 
-const DELIMITER: &[u8] = b"__END__";
-
 fn main() {
+    //Collect command
     let args: Vec<String> = args().collect();
 
+    //Build config and handle errors
     let config = Config::build_config(&args).unwrap_or_else(|err| {
-        println!("{}", err);
+        eprintln!("{}", err);
         std::process::exit(1)
     });
-
+    // Read input file and handle errors
     let input_string = lib::read_file(&config.input).unwrap_or_else(|err| {
-        println!("{:?}", err);
+        eprintln!("{:?}", err);
         std::process::exit(1)
     });
 
-    let frequency_table = lib::generate_frequency_table(&input_string);
+    let frequency_table = lib::generate_frequency_table(&input_string).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        std::process::exit(1);
+    });
 
-    let huffman_tree = lib::generate_huffman_tree(&frequency_table).unwrap();
-    println!("huffman tree{:?}", huffman_tree);
+    let huffman_tree = lib::generate_huffman_tree(&frequency_table).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        std::process::exit(1);
+    });
 
     let encoding_table = lib::generate_huffman_code(&huffman_tree);
-    println!("encoding_table  beofre {:?}", encoding_table);
+ 
+    lib::write_output(
+        &config.output,
+        &huffman_tree,
+        &input_string,
+        &encoding_table,
+    )
+    .unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        std::process::exit(1);
+    });
 
-    let (encoded_string, length) = encode_text(&input_string, &encoding_table);
-    println!("encoded_text   {}", encoded_string);
-    println!("length  {}", length);
+    lib::read_output(&config.output);
 
-    lib::write_output(&config.output, &huffman_tree, &encoded_string, length);
-
-    let deserialized_data = lib::read_output(&config.output);
-
-    // let deserialized_tree = lib::deserialize_tree(&deserialized_data);
-    // println!("deserialized tree    {:?}", deserialized_tree);
+    lib::compare_sizes(&config.input, &config.output);
 }
